2bf2fb0dd180aee2e4d90166a5e72983bdfa9a16
Elui Christian <eluichuma@gmail.com>
2019-04-10 09:00:46 +0100

    New db credentials

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'enudb.wdd.napier.ac.uk'),
-
-            'database' => env('DB_DATABASE', '40417376'),
-            'username' => env('DB_USERNAME', '40417376'),
-            'password' => env('DB_PASSWORD', '9JPBmyZ7'),
+            'host' => env('DB_HOST', 'db4free.net'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'chumadb1'),
+            'username' => env('DB_USERNAME', 'elui40417376'),
+            'password' => env('DB_PASSWORD', '070wayii'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
